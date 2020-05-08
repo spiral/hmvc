cf922247fdb89968024ce866c26145db6e3d3769
Valentin V / vvval <valentin@spiralscout.com>
2020-05-08 10:19:46 +0300

    Add Unauthenticated exception code

--- src/Exception/ControllerException.php
+++ src/Exception/ControllerException.php
@@ -19,10 +19,10 @@ class ControllerException extends RuntimeException
     /**
      * Pre-defined controller error codes.
      */
-    public const NOT_FOUND       = 0;
-    public const BAD_ACTION      = 1;
-    public const BAD_ARGUMENT    = 2;
-    public const FORBIDDEN       = 3;
-    public const ERROR           = 4;
-    public const UNAUTHENTICATED = 8;
+    public const NOT_FOUND    = 0;
+    public const BAD_ACTION   = 1;
+    public const BAD_ARGUMENT = 2;
+    public const FORBIDDEN    = 3;
+    public const ERROR        = 4;
+    public const UNAUTHORIZED = 8;
 }
